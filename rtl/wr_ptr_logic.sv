367aa88448ddbea045d828d75dcb25293ed7fc70
Saikrishna_Kolipaka <saikrishnakolipaka566@gmail.com>
2026-06-17 00:38:03 +0530

    Updated write pointer logic

--- rtl/wr_ptr_logic.sv
+++ rtl/wr_ptr_logic.sv
@@ -1,41 +1,62 @@
-module wr_ptr_logic# (parameter A_WIDTH = 4);
+module wr_ptr_logic# (parameter A_WIDTH = 4)
 
 (
    input 	wr_clk,
    input 	wr_rstn,
    input 	wr_en,
-   input [A_WIDTH : 0] gray_rd_ptr_sync,
+   input    [A_WIDTH : 0] gray_rd_ptr_sync,
 
-   output reg [A_WIDTH:0] gray_wr_ptr,
+   output   reg [A_WIDTH:0] g_wptr, //read synchronzer
    output 	full,
    output [A_WIDTH-1 : 0] wr_addr
-	);
 
-   reg [A_WIDTH : 0] bin_wr_ptr; //current state registers
-   
-   wire [A_WIDTH : 0] bin_wr_ptr_next; //next state wires
-   wire [A_WIDTH : 0] gray_wr_ptr_next;
+);
+  
+   wire [A_WIDTH : 0] b_wptr_next; //
+   wire [A_WIDTH : 0] g_wptr_next; //
+     
+   reg [A_WIDTH : 0] b_wptr;
 
+   assign b_wptr_next = b_wptr +(wr_en & !full);
+   assign g_wptr_next = b_wptr_next^(b_wptr_next >> 1);
 
-   assign gray_wr_ptr_next = bin_wr_ptr_next ^(bin_wr_ptr_next >> 1); // gray to bin conversion
-
-
-   always@(posedge wr_clk or negedge wr_rstn)
+   always@(posedge wr_clk or negedge wr_rstn) 
    begin
-	if(!wr_rstn)
-       	begin
-	     bin_wr_ptr <= 0;
-	     gray_wr_ptr <= 0;
-	end
+   	if(!wr_rstn) 
+		begin
+			b_wptr <= 0;
+			g_wptr <= 0;
+		end
 	else 
 	begin
-	 	if(wr_en && !full) bin_wr_ptr_next <= bin_wr_ptr + 1;
-		else bin_wr_ptr_next <= bin_wr_ptr;
-	     end
+		b_wptr <= b_wptr_next;
+		g_wptr <= g_wptr_next;
+	end
    end
+   
+ /*  always@(posedge wr_clk or negedge wr_rstn_
+   begin
+    if(
+   end
+*/
+  assign wr_addr = b_wptr[A_WIDTH -1 :0];
 
-
-  assign wr_addr = bin_wr_ptr[A_WIDTH -3 :0];
-  assign full = (gray_wr_ptr_next == {~gray_rd_ptr_sync[A_WIDTH : A_WIDTH -1], gray_rd_ptr_sync[A_WIDTH -2 : 0]});
+  assign full = (g_wptr_next == {~gray_rd_ptr_sync[A_WIDTH : A_WIDTH -1], gray_rd_ptr_sync[A_WIDTH -2 : 0]});
 
 endmodule
+
+/*
+depth = 0-6
+
+g_wptr // read domain
+b_wptr = 5 // memory  // 6 
+
+2) b_wptr = 6 
+
+b_wptr_next = 6
+
+g_wptr_next = 6 (gray)
+*/
+
+
+
