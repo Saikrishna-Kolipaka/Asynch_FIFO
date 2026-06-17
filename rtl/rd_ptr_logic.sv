e3f3154a2372dc07c8320c4de86b9192880662f1
Saikrishna_Kolipaka <saikrishnakolipaka566@gmail.com>
2026-06-17 23:36:16 +0530

    updated all logics

--- rtl/rd_ptr_logic.sv
+++ rtl/rd_ptr_logic.sv
@@ -5,17 +5,19 @@ module rd_ptr_logic# (parameter A_WIDTH = 4) //
    input rd_rstn,
    input [A_WIDTH : 0] g_wr_ptr_sync,
 
-   output empty,
+   output reg empty,
    output [A_WIDTH-1 : 0] rd_addr, 
    output reg [A_WIDTH : 0] g_rdptr
 	);
        
     reg [A_WIDTH : 0] b_rdptr;
+    
+	wire rempty;
 
     wire [A_WIDTH : 0] b_rdptr_next; 
     wire [A_WIDTH : 0] g_rdptr_next; 
  
-    assign b_rdptr_next = b_rdptr + (!empty & rd_en);
+    assign b_rdptr_next = b_rdptr + (!rempty & rd_en);
     assign g_rdptr_next = b_rdptr_next ^(b_rdptr_next >> 1);
 
     always@(posedge rd_clk or negedge rd_rstn) 
@@ -32,8 +34,14 @@ module rd_ptr_logic# (parameter A_WIDTH = 4) //
     end
     end
 
+   always@(posedge rd_clk or negedge rd_rstn) begin
+    if(!wrst_n) empty <= 1'b1;;
+    else        empty <= rempty;
+   end
+
+
     assign rd_addr = b_rdptr[A_WIDTH - 1 : 0];
 
-  assign empty = (g_rdptr_next == g_wr_ptr_sync);
+  assign rempty = (g_rdptr_next == g_wr_ptr_sync);
 
 endmodule
--- rtl/wr_ptr_logic.sv
+++ rtl/wr_ptr_logic.sv
@@ -7,7 +7,7 @@ module wr_ptr_logic# (parameter A_WIDTH = 4)
    input    [A_WIDTH : 0] g_rd_ptr_sync,
 
    output   reg [A_WIDTH:0] g_wptr, //read synchronzer
-   output 	full,
+   output 	reg full,
    output [A_WIDTH-1 : 0] wr_addr
 
 );
@@ -16,19 +16,12 @@ module wr_ptr_logic# (parameter A_WIDTH = 4)
    wire [A_WIDTH : 0] g_wptr_next; //
  
 
-   wire [A_WIDTH : 0] b_wptr_inc; //
-   wire [A_WIDTH : 0] g_wptr_inc; //
 
    reg [A_WIDTH : 0] b_wptr;
 
    wire wfull;
 
-   //prdict incremented pointer
-   assign b_wptr_inc = b_wptr + 1'b1;
-   assign g_wptr_inc = b_wptr_inc ^ (b_wptr_inc >> 1);
-
-
-   assign b_wptr_next = b_wptr +(wr_en & !wfull);
+   assign b_wptr_next = b_wptr +(wr_en & !wfull); // full -> b_wtr_nxt -> g_wtr_nxt ->full
    assign g_wptr_next = b_wptr_next^(b_wptr_next >> 1);
 
    always@(posedge wr_clk or negedge wr_rstn) 
@@ -44,12 +37,17 @@ module wr_ptr_logic# (parameter A_WIDTH = 4)
 		g_wptr <= g_wptr_next;
 	end
    end
+
+   always@(posedge wr_clk or negedge wr_rstn) begin
+    if(!wrst_n) full <= 0;
+    else        full <= wfull;
+   end
    
   assign wr_addr = b_wptr[A_WIDTH -1 :0];
 
-  assign wfull = (g_wptr_inc == {~g_rd_ptr_sync[A_WIDTH : A_WIDTH -1], g_rd_ptr_sync[A_WIDTH -2 : 0]});
+  assign wfull = (g_wptr_next == {~g_rd_ptr_sync[A_WIDTH : A_WIDTH -1], g_rd_ptr_sync[A_WIDTH -2 : 0]});
 
-  assign full = wfull;
+  //assign full = wfull;
 
 endmodule
 
@@ -65,18 +63,6 @@ b_wptr_next = 6
 
 g_wptr_next = 6 (gray)
 
-full  = current status
-
-wfull = "if I write now, will I become full?"
-
-b_wptr_inc    → predicted incremented pointer
-
-g_wptr_inc    → gray of predicted pointer
-
-wfull         → will fifo become full after increment?
-
-b_wptr_next   → actual next pointer
-g_wptr_next   → actual next gray pointer
 */
 
 
